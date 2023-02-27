ae61ba627484e657acebe855e37460464f3b4631
Albeiro Marin <xlandru@hotmail.com>
2023-02-27 14:43:00 -0500

    006 genera html automático y limpia la carpeta dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');   
 
 module.exports = {
     //archivo que se va a converitir (entry point)
-    entry: "./src/index.js",
+    //entry: "./src/index.js",
+
+    entry: {
+        index: "./src/index.js",
+        print: "./src/print.js"
+    },
+
+    //este pluguin se usa para crear de forma automática un archivo index.html,
+    //de forma dinámica agrega todos los scripts del bundle en el html
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Output Management',
+        }),
+    ],
 
     //archivo resultante después de la conversion
     output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, "dist"),
+        clean: true
     },
 };
