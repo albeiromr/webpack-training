4885b9409d06fbb19097a00c873744fc28e10ea3
Albeiro Marin <xlandru@hotmail.com>
2023-02-27 09:25:11 -0500

    001 bundle with webpack.config file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+//importando una librería externa en el script
+import _ from "lodash";
+
+const component = () => {
+    const element = document.createElement('div');
+ 
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+ 
+    return element;
+}
+ 
+document.body.appendChild(component());
