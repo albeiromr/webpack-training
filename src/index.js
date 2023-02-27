fefc8c5f94d986e1d6b0e19b28e8198c80676d1e
Albeiro Marin <xlandru@hotmail.com>
2023-02-27 10:15:55 -0500

    002 bundle css along with javascript

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 //importando una librería externa en el script
 import _ from "lodash";
+import './style.css';
 
 const component = () => {
     const element = document.createElement('div');
  
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
  
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require("path");
 
 module.exports = {
+    //archivo que se va a converitir (entry point)
     entry: "./src/index.js",
+
+    //archivo resultante después de la conversion
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+
+    //reglas y loaders que se usarán dentro de la configuración del archivo
+    module: {
+        rules: [
+            {
+                /* webpack uses a regular expression to determine which files it should look for and 
+                serve to a specific loader. In this case, any file that ends with .css will be served 
+                to the style-loader and the css-loader. */
+                test: /\.css$/i, //file extensions
+
+                /* Module loaders can be chained. Each loader in the chain applies transformations to the 
+                processed resource. A chain is executed in reverse order. The first loader passes its 
+                result (resource with applied transformations) to the next one, and so forth. Finally, 
+                webpack expects JavaScript to be returned by the last loader in the chain.
+
+                The above order of loaders should be maintained: 'style-loader' comes first and followed 
+                by 'css-loader'. If this convention is not followed, webpack is likely to throw errors. */
+                use: ['style-loader', 'css-loader'],//module loaders
+            }
+        ]
     }
 };
